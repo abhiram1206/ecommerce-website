a5d2fe265840e25f8d9c731272551ad46d066932
abhiram <abhiramputta12@gmail.com>
2024-02-13 19:42:00 +0530

    ecommerce website

---
+++ frontend/src/Pages/LoginSignup.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './css/LoginSignup.css'
+
+const LoginSignup = () => {
+  return (
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Enter Your Name' required/>
+          <input type="email" placeholder='Enter Your Email Id' required/>
+          <input type="password" placeholder='Enter Your Password' required/>
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login Here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
